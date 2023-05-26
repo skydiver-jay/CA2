a32317f054d2a21784efbbabf94d587a063ec5a8
Jay <chenjian_sun@hotmail.com>
2023-05-26 18:33:37 +0800

    新增注释，主要是理解CA2-SIM+版本与CA2基础版本的不同之处

--- CA2-SIM.py
+++ CA2-SIM.py
@@ -32,6 +32,8 @@ tf.flags.DEFINE_integer('image_width', 299, 'Width of each input images.')
 
 tf.flags.DEFINE_integer('image_height', 299, 'Height of each input images.')
 
+# 后续没有用到的全局变量，根据文章中实验章节的参数说明，该参数为DIM算法的参数
+#   组合环境中 DIM 方法的输入随机变换概率 = 0.7
 tf.flags.DEFINE_float('prob', 0.7, 'probability of using diverse inputs.')
 
 tf.flags.DEFINE_integer('image_resize', 331, 'Height of each input images.')
@@ -123,8 +125,10 @@ def grad_finish(x, x_ini, one_hot, i, grad):
     sample_num = FLAGS.sample_num
     return tf.less(i, sample_num)
 
+# DIM算法核心部分：每轮迭代均对输入图像进行随机的RESIZE和PADDING操作
 def input_diversity(input_tensor):
     rnd = tf.random_uniform((), FLAGS.image_width, FLAGS.image_resize, dtype=tf.int32)
+    # 获得随机数，后对输入样本进行随机resize变换
     rescaled = tf.image.resize_images(input_tensor, [rnd, rnd], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     h_rem = FLAGS.image_resize - rnd
     w_rem = FLAGS.image_resize - rnd
@@ -134,7 +138,12 @@ def input_diversity(input_tensor):
     pad_right = w_rem - pad_left
     padded = tf.pad(rescaled, [[0, 0], [pad_top, pad_bottom], [pad_left, pad_right], [0, 0]], constant_values=0.)
     padded.set_shape((input_tensor.shape[0], FLAGS.image_resize, FLAGS.image_resize, 3))
+
+    # 此处用到了DIM算法的关键参数prod，还没有理解其作用
+    #   随机数<prod，则使用ret=padded，即采用上面的resize&padded后的样本
+    #   随机数>=prod，则使用原输入样本，ret=input_tensor
     ret = tf.cond(tf.random_uniform(shape=[1])[0] < tf.constant(FLAGS.prob), lambda: padded, lambda: input_tensor)
+    # 但是在经过概率选择后，又进行了一次resize变换，重新调整会模型输入的要求大小，即有prob的概率进行变换，1-prod的概率不变换
     ret = tf.image.resize_images(ret, [FLAGS.image_height, FLAGS.image_width],
                                  method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     # DIM
@@ -163,6 +172,7 @@ def graph(x, x_ini, y, i, iternum, x_max, x_min, grad):
                                             [x, x_ini, one_hot, num, tf.zeros_like(x)])
 
     # TIM
+    # 这里由于引入TIM算法策略，使用的卷积核尺寸为11，和基础版本的CA2不一致，可以对比基础版本CA2的代码
     noise = tf.nn.depthwise_conv2d(noise, stack_kernel, strides=[1, 1, 1, 1], padding='SAME')
     noise = noise / tf.reduce_mean(tf.abs(noise), [1, 2, 3], keep_dims=True)
     noise = momentum * grad + noise
@@ -193,12 +203,15 @@ def compute_grads(x, x_ini, one_hot, i, grad):
     vector = tf.random_normal(shape=x.shape)
 
     # cyclical augmentation (deviation-augmentation)
+    # sample_variance为偏移距离（文章中的w），可以理解为图像每个像素的偏移量
+    # 通过sign函数符号化偏移向量，可以控制每像素偏移值为 -w、0、w
     x_nes = x + FLAGS.sample_variance * tf.sign(vector)
 
     logits, end_points = get_logits(x_nes, model)
     n = 1
 
     # SIM
+    # 此处为引入SIM算法思想的核心，即对偏移增强后的样本在每个像素上做数值缩放（每个像素除以一个整数2^i & i=1/2/3，即如下代码中的2/4/8），再计算梯度
     x_nes_2 = 1 / 2 * x_nes
     logits_2, end_points_2 = get_logits(x_nes_2, model)
 
