804cad5c1290ea9cb4f8a10d14bf95152340f9b7
Jay <chenjian_sun@hotmail.com>
2023-06-02 11:27:33 +0800

    基于MIM-RO-V3，整理出了可发布到tf2分支的CA2基础版本实现，demo验证无问题

--- TF2_dev/conf_basic.py
+++ TF2_dev/conf_basic.py
@@ -1,15 +1,19 @@
 """CA2基础班TF2版本 配置文件"""
 
-import numpy as np
-from TF2_dev.utils_tf2 import *
-import random
+from utils_tf2 import *
 
 """
 此配置文件仅包括CA2中RO和SA相关配置。
 本地模型和图像相关配置使用单独配置文件。
 """
 
-# 原图最大扰动范围，文中实验设置为12.0，但在ImageNet数据集和ResNet模型上实验结果噪声过于明显。
+# 仅当不需要真随机场景时，将need_true_random配置为False
+need_true_random = True
+seed = 0
+if not need_true_random:
+    tf.random.set_seed(seed)
+
+# 原图最大扰动范围，CA2文中实验设置为12.0，但在ImageNet数据集和ResNet模型上实验结果噪声过于明显。
 # 本地实验显示，设置为3.0，获得的对抗样本噪声几乎不影响视觉效果。
 max_epsilon = 3.0
 
@@ -18,11 +22,11 @@ momentum_decay_factor = 1.0
 
 
 ##########################           偏移增强D2A策略配置           ##########################
-# sample_num: 属于CA2框架“偏移增强”特性的一个参数。 文章中的k，生成偏移增强样本的数量，也即是随机采样的数量
+# sample_num: 属于CA2框架“偏移增强”策略的一个参数。 文章中的k，生成偏移增强样本的数量，也即是随机采样的数量
 # sample_variance: 为偏移距离，即文章中的ω，文章中实验设置为0.05，此处设置为0.1
-#   CA2.py中使用0.1，是因为本地模型为inception，样本归一化到(-1,1)
-#   文中中使用0.05，是因为在(0,1)语境下表述
-#   此处采用0.05，那么在D2A策略实现代码中，需要根据输入样本的bounds将样本归一化到(0,1)后再进行计算
+#   TF1版本CA2.py中使用0.1，是因为本地模型为inception，样本归一化到(-1,1)
+#   文章中使用0.05，是因为在(0,1)语境下表述
+#   此处采用0.05，那么在D2A策略实现代码中，样本的bounds将样本归一化到(0,1)后再进行计算
 sample_num = 4
 sample_variance = 0.05
 
@@ -35,14 +39,14 @@ iteration_num = 16
 momentum_learn_factor = 1.0
 
 ##########################           虚拟集成VME策略配置           ##########################
-# 配置卷积核尺寸分别为9和11的两个高斯卷积矩阵
+# 配置卷积核尺寸分别为1/9/11的高斯卷积矩阵
 #   文章中结论：选择卷积核尺寸集合为 {1,9,11}，默认情况下建议使用此配置；当卷积核尺寸为1时，实则为原模型不变，这个在后续全局梯度计算相关代码中有体现
 list_stack_kernel_size = [1, 9, 11]
 # 如果需要自定义，只需要变更上一行数组中的奇数配置即可，以下3行代码无需变动
-stack_kernel = gkern(list_stack_kernel_size[0], 1)
-stack_kernel_9 = gkern(list_stack_kernel_size[1], 3)
-stack_kernel_11 = gkern(list_stack_kernel_size[2], 3)
-stack_kernel_list = [stack_kernel, stack_kernel_9, stack_kernel_11]
+stack_kernel_1 = gkern(list_stack_kernel_size[0], 3)  # 实际上不起作用
+stack_kernel_2 = gkern(list_stack_kernel_size[1], 3)
+stack_kernel_3 = gkern(list_stack_kernel_size[2], 3)
+stack_kernel_list = [stack_kernel_1, stack_kernel_2, stack_kernel_3]
 
 # 参数合法性检查
 if phase_num != len(phase_step):
@@ -71,5 +75,7 @@ print_global_conf(sample_variance, "D2A 之 偏移距离")
 
 print_global_conf(list_stack_kernel_size, "VME 之 卷积核尺寸配置")
 
+print_global_conf(tf.__version__, "TensorFlow版本")
 
-
+if not need_true_random:
+    print_global_conf(seed, "随机种子")
--- TF2_dev/utils_tf2.py
+++ TF2_dev/utils_tf2.py
@@ -1,10 +1,11 @@
 import numpy as np
+import tensorflow as tf
 
 
 def gkern(kernlen=21, nsig=3):
     """
     Returns a 2D Gaussian kernel array.
-    这是一个副本，来自于原TF1版本中utils.py
+    这是一个副本，来自于原CA2 TF1版本中utils.py
     """
 
     import scipy.stats as st
@@ -20,6 +21,97 @@ def gkern(kernlen=21, nsig=3):
     return stack_kernel
 
 
+def optimize_linear(grad, eps, norm=np.inf):
+    """
+    这是来自cleverhans项目的一个副本
+
+    项目地址：https://github.com/cleverhans-lab/cleverhans/blob/master/cleverhans/tf2/utils.py
+
+    Solves for the optimal input to a linear function under a norm constraint.
+
+    Optimal_perturbation = argmax_{eta, ||eta||_{norm} < eps} dot(eta, grad)
+
+    :param grad: tf tensor containing a batch of gradients
+    :param eps: float scalar specifying size of constraint region
+    :param norm: int specifying order of norm
+    :returns:
+      tf tensor containing optimal perturbation
+    """
+
+    # Convert the iterator returned by `range` into a list.
+    axis = list(range(1, len(grad.get_shape())))
+    avoid_zero_div = 1e-12
+    if norm == np.inf:
+        # Take sign of gradient
+        optimal_perturbation = tf.sign(grad)
+        # The following line should not change the numerical results. It applies only because
+        # `optimal_perturbation` is the output of a `sign` op, which has zero derivative anyway.
+        # It should not be applied for the other norms, where the perturbation has a non-zero derivative.
+        optimal_perturbation = tf.stop_gradient(optimal_perturbation)
+    elif norm == 1:
+        abs_grad = tf.abs(grad)
+        sign = tf.sign(grad)
+        max_abs_grad = tf.reduce_max(abs_grad, axis, keepdims=True)
+        tied_for_max = tf.dtypes.cast(
+            tf.equal(abs_grad, max_abs_grad), dtype=tf.float32
+        )
+        # tf.reduce_sum: https://www.w3cschool.cn/tensorflow_python/tensorflow_python-5y4d2i2n.html
+        num_ties = tf.reduce_sum(tied_for_max, axis, keepdims=True)
+        optimal_perturbation = sign * tied_for_max / num_ties
+    elif norm == 2:
+        square = tf.maximum(
+            avoid_zero_div, tf.reduce_sum(tf.square(grad), axis, keepdims=True)
+        )
+        optimal_perturbation = grad / tf.sqrt(square)
+    else:
+        raise NotImplementedError(
+            "Only L-inf, L1 and L2 norms are currently implemented."
+        )
+
+    # Scale perturbation to be the solution for the norm=eps rather than norm=1 problem
+    scaled_perturbation = tf.multiply(eps, optimal_perturbation)
+    return scaled_perturbation
+
+
+def clip_eta(eta, norm, eps):
+    """
+    这是来自cleverhans项目的一个副本
+
+    项目地址：https://github.com/cleverhans-lab/cleverhans/blob/master/cleverhans/tf2/utils.py
+
+    Helper function to clip the perturbation to epsilon norm ball.
+    :param eta: A tensor with the current perturbation.
+    :param norm: Order of the norm (mimics Numpy).
+                Possible values: np.inf, 1 or 2.
+    :param eps: Epsilon, bound of the perturbation.
+    """
+
+    # Clipping perturbation eta to self.norm norm ball
+    if norm not in [np.inf, 1, 2]:
+        raise ValueError("norm must be np.inf, 1, or 2.")
+    axis = list(range(1, len(eta.get_shape())))
+    avoid_zero_div = 1e-12
+    if norm == np.inf:
+        eta = tf.clip_by_value(eta, -eps, eps)
+    else:
+        if norm == 1:
+            raise NotImplementedError("")
+            # This is not the correct way to project on the L1 norm ball:
+            # norm = tf.maximum(avoid_zero_div, reduce_sum(tf.abs(eta), reduc_ind, keepdims=True))
+        elif norm == 2:
+            # avoid_zero_div must go inside sqrt to avoid a divide by zero in the gradient through this operation
+            norm = tf.sqrt(
+                tf.maximum(
+                    avoid_zero_div, tf.reduce_sum(tf.square(eta), axis, keepdims=True)
+                )
+            )
+        # We must *clip* to within the norm ball, not *normalize* onto the surface of the ball
+        factor = tf.minimum(1.0, tf.math.divide(eps, norm))
+        eta = eta * factor
+    return eta
+
+
 if __name__ == "__main__":
-    stack_kernel_9 = gkern(9, 3)
-    print(stack_kernel_9.shape)
+    stack_kernel = gkern(1, 3)
+    print(stack_kernel.shape)
+    print(stack_kernel)
